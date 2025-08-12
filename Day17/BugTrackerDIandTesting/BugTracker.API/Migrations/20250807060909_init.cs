f0507ed5a75a0bb251c22f9d3777ed0da2f110b8
pradeep-krishnaa <pradeepkrishnaa.2205108@srec.ac.in>
2025-08-12 10:48:02 +0530

    feat: added PUT and DELETE endpoints to BugTracker

---
+++ Day17/BugTrackerDIandTesting/BugTracker.API/Migrations/20250807060909_init.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugTracker.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bugs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bugs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bugs_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bugs_ProjectId",
+                table: "Bugs",
+                column: "ProjectId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bugs");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
---
+++ Day17/BugTrackerDIandTesting/BugTracker.Application/Services/BugService.cs
@@ -0,0 +1,87 @@
+﻿using BugTracker.Core.DTOs;
+using BugTracker.Core.Entities;
+using BugTracker.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Application.Services
+{
+    public class BugService : IBugService
+    {
+        private readonly IBugRepository _bugRepository;
+        public int _nextId = 1; // This is just a placeholder for ID generation, not used in this implementation.
+        public BugService(IBugRepository bugRepository)
+        {
+            _bugRepository = bugRepository;
+        }
+
+        public void CreateBug(BugRequestDTO bug)
+        {
+            var newBug = new Bug
+            {
+                Id = _nextId++, // Simulating ID generation
+                Title = bug.Title,
+                Description = bug.Description,
+                Status = bug.Status,
+                ProjectId = bug.ProjectId
+            };
+            _bugRepository.Add(newBug);
+            _bugRepository.Save();
+        }
+        public List<BugResponseDTO> GetAllBugs()
+        {
+            var bugs = _bugRepository.GetAll();
+            return bugs.Select(b => new BugResponseDTO
+            {
+                Id = b.Id,
+                Title = b.Title,
+                Description = b.Description,
+                Status = b.Status,
+                ProjectId = b.ProjectId
+            }).ToList();
+        }
+
+        public BugResponseDTO GetBugById(int id)
+        {
+            var bug = _bugRepository.GetById(id);
+            if (bug == null) return null;
+            return new BugResponseDTO
+            {
+                Id = bug.Id,
+                Title = bug.Title,
+                Description = bug.Description,
+                Status = bug.Status,
+                ProjectId = bug.ProjectId
+            };
+        }
+        public void UpdateBug(int id , BugRequestDTO bug)
+        {
+            var existingBug = _bugRepository.GetById(id);
+            if (existingBug != null)
+            {
+                existingBug.Title = bug.Title;
+                existingBug.Description = bug.Description;
+                existingBug.Status = bug.Status;
+                existingBug.ProjectId = bug.ProjectId;
+                _bugRepository.Update(existingBug);
+                _bugRepository.Save();
+            }
+
+        }
+        
+
+        public void DeleteBug(int id)
+        {
+            var bug = _bugRepository.GetById(id);
+            if (bug != null)
+            {
+                _bugRepository.Delete(id);
+                _bugRepository.Save();
+            }
+        }
+    }
+
+}
